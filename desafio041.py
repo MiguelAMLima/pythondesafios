ff42bd4cf9c5a5ae0370b3e5cbcd32baf6ca3c34
Miguel Ângelo Melo de Lima <127374796+MiguelAMLima@users.noreply.github.com>
2023-03-17 04:18:42 -0300

    Desafios adicionados

---
+++ desafio041.py
@@ -0,0 +1,4 @@
+# A Confederação Nacional de Natação precisa de um programa que leia o ano
+# de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
+# -Até 9 anos: mirim; -Até 14 anos: infantil; -Até 19 anos: júnior;
+# -Até 20 anos: sênior; -Acima: master.
