705eacf224ed215c3b5492f3d406fd0e967f0b49
Miguel Ângelo Melo de Lima <127374796+MiguelAMLima@users.noreply.github.com>
2023-04-18 00:09:08 -0300

    Desafios resolvidos até o 037

--- desafio036.py
+++ desafio036.py
@@ -2,3 +2,13 @@
 # casa. O programa vai perguntar o valor da casa, o salário do comprador e em
 # quantos anos ele vai pagar. Calcule o valor da prestação mensal, sabendo que
 # não pode exceder 30% do salário ou então o empréstimo será negado.
+print('Bem-vindo ao programa de análise de empréstimos, para começar:')
+valor = float(input('Informe o valor da casa que deseja comprar: '))
+salario = float(input('Informe o salário do comprador: '))
+tempo = int(input('Informe em quantos anos deseja pagar: '))
+prestacao = valor / (tempo * 12)
+if prestacao > ((30 / 100) * salario):
+    print('O valor da prestação mensal foi de R$:{:.2f} e ultrapassou 30% do seu salário,'
+          ' portanto seu empréstimo foi negado!'.format(prestacao))
+else:
+    print('Seu empréstimo foi aprovado! o valor da prestação mensal será de R$:{:.2f}'.format(prestacao))
