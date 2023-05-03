b772407dc93f1ceb918f5067a3a00ac272d35de1
Miguel Ângelo Melo de Lima <127374796+MiguelAMLima@users.noreply.github.com>
2023-05-02 22:54:55 -0300

    Desafios resolvidos até o 045

--- desafio044.py
+++ desafio044.py
@@ -2,3 +2,21 @@
 # considerando o seu preço normal e condição de pagamento: -À vista
 # dinheiro/cheque: 10% de desconto; -À vista no cartão: 5% de desconto;
 # -Em até 2x no cartão: preço normal; -3x ou mais no cartão: 20% de juros.
+preco = float(input('Preço do produto: R$ '))
+print('Escolha a forma de pagamento:\n')
+print('[ 1 ] À vista no dinheiro/cheque\n[ 2 ] À vista no cartão'
+      '\n[ 3 ] Em até 2x no cartão\n[ 4 ] 3x ou mais no cartão\n')
+escolha = int(input('Digite o número da opção desejada: '))
+if escolha == 1:
+    preco = preco - (10/100 * preco)
+    print('Você ganhou 10% de desconto e o valor final será de R$ {:.2f}'.format(preco))
+elif escolha == 2:
+    preco = preco - (5/100 * preco)
+    print('Você ganhou 5% de desconto e o valor final será de R$ {:.2f}'.format(preco))
+elif escolha == 3:
+    print('O valor final do produto será de R$ {:.2f}'.format(preco))
+elif escolha == 4:
+    preco = preco + (20/100 * preco)
+    print('A compra terá juros de 20% e o valor final será de R$ {:.2f}'.format(preco))
+else:
+    print('Você não digitou nenhuma opção válida, tente novamente!')
--- desafio045.py
+++ desafio045.py
@@ -1,1 +1,29 @@
 # Crie um programa que faça o computador jogar Jokenpô com você.
+from random import randint
+print('\nVamos jogar Jokenpô!!\n')
+print('Escolha:\n[ 1 ] para pedra\n[ 2 ] para papel\n[ 3 ] para tesoura\n')
+jogador = int(input('Sua escolha: '))
+pc = randint(1, 3)
+if jogador == 1:
+    if pc == 1:
+        print('Você escolheu PEDRA e eu escolhi PEDRA...\nTemos um empate!')
+    elif pc == 2:
+        print('Você escolheu PEDRA e eu escolhi PAPEL...\nEu venci!! :D')
+    elif pc == 3:
+        print('Você escolheu PEDRA e eu escolhi TESOURA...\nParabéns, você me venceu!!')
+elif jogador == 2:
+    if pc == 1:
+        print('Você escolheu PAPEL e eu escolhi PEDRA...\nParabéns, você me venceu!!')
+    elif pc == 2:
+        print('Você escolheu PAPEL e eu escolhi PAPEL...\nTemos um empate!')
+    elif pc == 3:
+        print('Você escolheu PAPEL e eu escolhi TESOURA...\nEu venci!! :D')
+elif jogador == 3:
+    if pc == 1:
+        print('Você escolheu TESOURA e eu escolhi PEDRA...\nEu venci!! :D')
+    elif pc == 2:
+        print('Você escolheu TESOURA e eu escolhi PAPEL...\nParabéns, você me venceu!!')
+    elif pc == 3:
+        print('Você escolheu TESOURA e eu escolhi TESOURA...\nTemos um empate!')
+else:
+    print('Você não escolheu nenhuma opção válida, tente novamente!')
