0068e568393633d87fd5f272fafebf21d2286a38
Miguel Ângelo Melo de Lima <127374796+MiguelAMLima@users.noreply.github.com>
2023-05-24 23:54:55 -0300

    Desafios resolvidos até o 065

--- desafio064.py
+++ desafio064.py
@@ -1,3 +1,10 @@
 # Crie um programa que leia vários números inteiros pelo teclado. O programa
 # só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
 # No final, mostre quantos números foram digitados e qual foi a soma entre eles.
+valor = soma = cont = 0
+while valor != 999:
+    valor = int(input('Informe um valor para a soma [Parada: 999]: '))
+    if valor != 999:
+        soma += valor
+        cont += 1
+print(f'Você informou {cont} valores e a soma entre eles é {soma}.')
--- desafio065.py
+++ desafio065.py
@@ -2,3 +2,18 @@
 # execução, mostre a média entre todos os valores e qual foi o maior e o menor
 # valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar
 # a digitar valores.
+num = int(input('Digite um valor inteiro: '))
+soma = maior = menor = num
+cont = 1
+continuar = str(input('Deseja continuar digitando valores? [S/N]: ')).strip().upper()[0]
+while continuar != 'N':
+    num = int(input('Digite um valor inteiro: '))
+    soma += num
+    cont += 1
+    if num > maior:
+        maior = num
+    if num < menor:
+        menor = num
+    continuar = str(input('Deseja continuar digitando valores? [S/N]: ')).strip().upper()[0]
+print(f'\nO maior valor digitado foi: {maior}\nO menor valor digitado foi: {menor}'
+      f'\nA média entre os {cont} valores digitados foi: {(soma / cont):.2f}')
