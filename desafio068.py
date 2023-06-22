ef192b90328092e9fb04aa9cd3b67c08f918ba72
NATI-PROGEST <m.angelomdl97@gmail.com>
2023-06-22 10:16:42 -0300

    Desafios resolvidos até o 068

--- desafio068.py
+++ desafio068.py
@@ -7,21 +7,21 @@ print('-' * 30)
 print('Vamos jogar PAR ou ÍMPAR!')
 print('-' * 30)
 while True:
-    jogador1 = int(input('[0 = PAR / 1 = ÍMPAR] Digite a sua escolha: '))
-    jogador2 = int(input('Escolha um número de 0 a 10: '))
+    tipo = int(input('[0 = PAR / 1 = ÍMPAR] Digite a sua escolha: '))
+    jogador = int(input('Escolha um número de 0 a 10: '))
     computador = randint(0, 10)
-    soma = jogador2 + computador
+    soma = jogador + computador
     print(f'Meu número foi: {computador}')
     print('-' * 30)
     if soma % 2 == 0:
-        if jogador1 == 0:
+        if tipo == 0:
             print(f'Você escolheu PAR e o valor foi {soma}. Parabéns, você ganhou! vamos de novo!')
             cont += 1
         else:
             print(f'Você escolheu ÍMPAR e o valor foi {soma}. EU GANHEI!')
             break
-    if soma % 2 != 0:
-        if jogador1 == 0:
+    else:
+        if tipo == 0:
             print(f'Você escolheu PAR e o valor foi {soma}. EU GANHEI!')
             break
         else:
