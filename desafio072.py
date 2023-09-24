e44c94c184a09a56b599f8d59e108a75bd47d082
Miguel Ângelo Melo de Lima <127374796+MiguelAMLima@users.noreply.github.com>
2023-09-24 01:07:10 -0300

    Desafio 72 resolvido

--- desafio072.py
+++ desafio072.py
@@ -2,3 +2,17 @@
 # com uma contagem por extenso, de zero até vinte. Seu programa
 # deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por
 # extenso.
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
+           'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze',
+           'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+repetir = 'S'
+while repetir == 'S':
+    while True:
+        num = int(input('Digite um número entre 0 e 20 para visualizá-lo por extenso: '))
+        if 0 <= num <= 20:
+            break
+        print('Tente novamente. ', end='')
+    print(f'O número {num} por extenso é: {extenso[num]}')
+    repetir = input('Deseja ver outro número? [S/N] ').upper()[0]
+print('-=' * 16)
+print('Programa encerrado.')
