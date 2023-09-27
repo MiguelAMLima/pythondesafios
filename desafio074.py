510f0a5cd2f411a62d42d7eeb599fd3bf6f8a972
Miguel Ângelo Melo de Lima <127374796+MiguelAMLima@users.noreply.github.com>
2023-09-26 23:37:23 -0300

    Desafio 74 resolvido

--- desafio074.py
+++ desafio074.py
@@ -1,3 +1,9 @@
 # Crie um programa que vai gerar cinco números aleatórios e colocar
 # em uma tupla. Depois disso, mostre a listagem de números gerados e
 # também indique o menor e o maior valor que estão na tupla.
+from random import randint
+num = (randint(1, 1000), randint(1, 1000), randint(1, 1000),
+       randint(1, 1000), randint(1, 1000))
+print(f'Números gerados aleatoriamente: {num}'
+      f'\nO maior valor da lista é: {max(num)}'
+      f'\nO menor valor da lista é: {min(num)}')
