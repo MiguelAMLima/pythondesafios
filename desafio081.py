d0c40964fea97696cc169f5190cfb0cee174a8b1
Miguel Ângelo Melo de Lima <127374796+MiguelAMLima@users.noreply.github.com>
2023-10-05 19:29:42 -0300

    Desafios adicionados até o 083

---
+++ desafio081.py
@@ -0,0 +1,4 @@
+# Crie um programa que vai ler vários números e colocá-los em uma lista.
+# Depois disso, mostre: A) Quantos números foram digitados. B) A lista
+# de valores, ordenada de forma decrescente. C) Se o valor 5 foi digitado
+# e está ou não na lista.
---
+++ desafio082.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia vários números e coloque-os em uma lista.
+# Depois disso, crie duas listas extras que vão conter apenas os valores
+# pares e os valores ímpares digitados, respectivamente. Ao final, mostre
+# o conteúdo das 3 listas geradas.
