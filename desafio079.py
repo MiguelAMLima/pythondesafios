a1bd234a14294167678e999a2e8c1299b29effb9
MiguelAMLima <m.angelomdl97@gmail.com>
2023-11-17 20:13:43 -0300

    Desafios resolvidos até o 079

--- desafio079.py
+++ desafio079.py
@@ -2,3 +2,17 @@
 # e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não
 # será adicionado. No final, serão exibidos todos os valores únicos
 # digitados, em ordem crescente.
+listanum = []
+while True:
+    num = float(input('Digite um valor: '))
+    if num not in listanum:
+        listanum.append(num)
+        print(f'Valor {num} adicionado à lista!')
+    else:
+        print(f'O valor {num} já foi digitado, não será adicionado à lista novamente.')
+    continuar = str(input('Deseja continuar? [S/N]: ')).strip()[0]
+    if continuar in 'Nn':
+        break
+print('=-' * 30)
+listanum.sort()
+print(f'Valores digitados em ordem crescente: {listanum}')
