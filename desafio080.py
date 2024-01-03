5f6387f9d375347a0a5830b47e4461efe779a1d2
MiguelAMLima <m.angelomdl97@gmail.com>
2024-01-03 03:15:58 -0300

    Desafios resolvidos até o 081

--- desafio080.py
+++ desafio080.py
@@ -1,3 +1,19 @@
 # Crie um programa onde o usuário possa digitar 5 valores numéricos
 # e cadastre-os em uma lista, já na posição correta de inserção
 # (sem usar o sort()). No final, mostre a lista ordenada na tela.
+listanum = []
+for c in range(1, 6):
+    valor = float(input(f'Digite o {c}º valor: '))
+    if c == 1 or valor > listanum[-1]:
+        listanum.append(valor)
+        print(f'{valor} foi adicionado ao fim da lista.')
+    else:
+        pos = 0
+        while pos < len(listanum):
+            if valor <= listanum[pos]:
+                listanum.insert(pos, valor)
+                print(f'{valor} adicionado na posição {pos} da lista.')
+                break
+            pos += 1
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram: {listanum}')
--- desafio081.py
+++ desafio081.py
@@ -2,3 +2,17 @@
 # Depois disso, mostre: A) Quantos números foram digitados. B) A lista
 # de valores, ordenada de forma decrescente. C) Se o valor 5 foi digitado
 # e está ou não na lista.
+listanum = []
+while True:
+    listanum.append(float(input(f'Digite o {len(listanum) + 1}º valor: ')))
+    continuar = str(input('Deseja continuar? [S/N] ')).strip()[0]
+    if continuar in 'Nn':
+        break
+listanum.sort(reverse=True)
+print('-=' * 30)
+print(f'Foram digitados {len(listanum)} valores.')
+print(f'Lista de valores em ordem decrescente: {listanum}')
+if 5 in listanum:
+    print(f'O valor 5 foi digitado e adicionado à lista {listanum.count(5)}x')
+else:
+    print('O valor 5 não foi digitado, portanto, não está na lista.')
