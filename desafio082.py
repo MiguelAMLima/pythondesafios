7553b30e7baf73fde54f94ef91c7e9bcf69b6101
Miguel Lima <m.angelomdl97@gmail.com>
2024-07-02 19:57:39 -0300

    Desafios resolvidos até o 083

--- desafio082.py
+++ desafio082.py
@@ -2,3 +2,20 @@
 # Depois disso, crie duas listas extras que vão conter apenas os valores
 # pares e os valores ímpares digitados, respectivamente. Ao final, mostre
 # o conteúdo das 3 listas geradas.
+num = list()
+par = list()
+impar = list()
+while True:
+    num.append(int(input('Digite um número: ')))
+    continuar = str(input('Deseja continuar? [S/N] ')).strip()[0]
+    if continuar in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        par.append(v)
+    elif v % 2 == 1:
+        impar.append(v)
+print('-=' * 24)
+print(f'A lista completa é: {num}')
+print(f'A lista de pares é: {par}')
+print(f'A lista de ímpares é: {impar}')
--- desafio083.py
+++ desafio083.py
@@ -1,3 +1,18 @@
 # Crie um programa onde o usuário digite uma expressão matemática qualquer
 # que use parênteses. Seu aplicativo deverá analisar se a expressão passada
 # está com os parênteses abertos e fechados na ordem correta.
+expmat = str(input('Digite a expressão matemática: '))
+pilha = []
+for simb in expmat:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
