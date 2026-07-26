e3715e9782982bdbc82d45eea9ffbdbbf57bc409
Miguel Lima <m.angelomdl97@gmail.com>
2026-07-25 23:30:31 -0300

    Desafios resolvidos até o 095

--- desafio094.py
+++ desafio094.py
@@ -3,3 +3,40 @@
 # dicionários em uma lista. No final, mostre: A) Quantas pessoas foram
 # cadastradas. B) A média de idade do grupo. C) Uma lista com todas as
 # mulheres. D) Uma lista com todas as pessoas com idade acima da média.
+galera = list()
+pessoa = dict()
+soma = media = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'Ao todo temos {len(galera)} pessoas cadastradas.\n')
+media = soma / len(galera)
+print(f'A média de idade é de {media:5.2f} anos.\n')
+print('As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]}, ', end='')
+print('\n')
+print('Pessoas com idade acima da média: ')
+for p in galera:
+    if p['idade'] > media:
+        print('    ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
